92c4361625a8b87114f4fa2ee70a555bcea68cf7
Aseel <aseelmahmood12@gmail.com>
2025-11-22 20:12:08 +0100

    Initial commit

---
+++ Nepkey-Extension-Safari/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Nepkey-Extension-Safari
+//
+//  Created by Aseel Yousif on 11/22/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
